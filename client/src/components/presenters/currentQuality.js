a8a13455ced9e0d12cb37df0e816319c60b6a49c
smalmb <smalmb@kth.se>
2021-04-29 17:04:11 +0200

    The presenter now asks the database for data every 5 seconds

--- client/src/components/presenters/currentQuality.js
+++ client/src/components/presenters/currentQuality.js
@@ -1,6 +1,13 @@
 import React from "react";
 import {CurrentQualityView} from '../views/currentQualityView';
 
+
+/**
+ * The functions parses the data it is given to a simple object
+ * 
+ * @param {*} data the data to be parsed
+ * @returns an object which contains the parsed data
+ */
 function parser(data){
     return {
         date: data["docs"][0]["_id"],
@@ -11,13 +18,30 @@ function parser(data){
 export const CurrentQualityPage = () => {
 
     const [data, setData] = React.useState(null);
+    const [fetching, setShouldFetch] = React.useState(true);
 
+
+    function toggle(){
+        setShouldFetch(!fetching)
+    }
+   
+    //React hook which will run every 5 seconds
+    //The hook will fetch data from the datbase and set the state variabel data to the data which was fetched
     React.useEffect(() => {
+
         fetch('/api/sensor/airquality')
         .then((res) => res.json())
         .then((json) => parser(json))
         .then((parsed) => setData(parsed))
-    }, [])
+
+        const intervalId = setInterval(() => {
+          toggle()
+        }, 5000);
+
+        return () => clearInterval(intervalId)
+
+    }, [fetching])
+
 
     return (
     React.createElement(CurrentQualityView, 
