25e0d59eba973a602c352ac54b43a34a16642a78
lvndquist <jonatanlundqvistsilins@gmail.com>
2021-05-17 11:55:13 +0200

    documented main program

--- Hardware/OEM/Core/Inc/office_environment_monitor.h
+++ Hardware/OEM/Core/Inc/office_environment_monitor.h
@@ -36,66 +36,85 @@ typedef enum
 } RETURN_STATUS;
 
 /**
- *
+ * @brief initiate the display
+ * @param void
+ * @return void
  */
 void display_startscreen(void);
 
 /**
- *
+ * @brief waits until data from the CCS811 sensor is available. Implements a loading animation.
+ * @param void
+ * @return void
  */
 void display_getting_data_screen(void);
 
 /**
- *
+ * @brief shows the temperature, humidity, co2 and tVoc values on the display
+ * @param float temp, the temperature
+ * @param float hum, the humidity
+ * @param uint16_t co2, the CO2 value
+ * @param uint16_t tVoc, the Tvoc value
+ * @return void
  */
 void show_measurements(float temp, float hum, uint16_t co2, uint16_t tVoc);
 
 /**
- *
+ * @brief main program
+ * @param void
+ * @return void
  */
 void office_environment_monitor(void);
 
 /**
- *
+ * @brief error handler, which handles all errors defined in the RETURN_STATUS enum.
+ * 		  Most errors will just print some message to the display and then freeze.
+ * @param void
+ * @return void
  */
 void error_handler(void);
 
 /**
- *
- */
-RETURN_STATUS get_return_status(void);
-
-/**
- *
- */
-void set_return_status(RETURN_STATUS status_code);
-
-/**
- *
+ * @brief initiates the esp8266. Enables interrupts for uart4 and checks the return status.
+ * @param void
+ * @return RETURN_STATUS, either ESP8266_START_ERROR or ESP8266_START_SUCCESS
  */
 RETURN_STATUS esp8266_start(void);
 
 /**
- *
+ * @brief connects the esp8266 to wifi, and checks the return status.
+ * @param void
+ * @return RETURN_STATUS, either ESP8266_WIFI_CON_ERROR or ESP8266_WIFI_CON_SUCCESS
  */
 RETURN_STATUS esp8266_wifi_start(void);
 
 /**
- *
+ * @brief establishes a tcp connection to the project website.
+ * @param void
+ * @return RETURN_STATUS, either ESP8266_WEB_DISCONNECTED or ESP8266_WEB_CONNECTED
  */
 RETURN_STATUS esp8266_web_connection(void);
 
 /**
- *
+ * @brief sends a http post request to the project website. Data passed in the parameters are posted to the website.
+ * @param uint16_t co2, CO2 value
+ * @param uint16_t tvoc, tVOC value
+ * @param float temp, temperature value
+ * @param float hum, humidity value
+ * @return RETURN_STATUS, either ESP8266_WEB_REQUEST_ERROR or ESP8266_WEB_REQUEST_SUCCESS
  */
 RETURN_STATUS esp8266_web_request(uint16_t co2, uint16_t tvoc, float temp, float hum);
 
 /**
- *
+ * @brief initiates the ccs811 with all settings needed for environmental measurements each second.
+ * @param void
+ * @return RETURN_STATUS, either CCS811_START_ERROR or CCS811_START_SUCCESS
  */
 RETURN_STATUS ccs811_start(void);
 
 /**
- *
+ * @brief initiates the bme280 to measure temperature and humidity.
+ * @param void
+ * @return RETURN_STATUS, either BME280_START_ERROR or BME280_START_SUCCESS.
  */
 RETURN_STATUS bme280_start(void);
